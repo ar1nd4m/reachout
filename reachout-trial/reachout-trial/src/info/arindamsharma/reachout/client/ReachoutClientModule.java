03c9d56bba7ef6ef8c1ff425430335e0d1ee40b4
Arindam Sharma <arindam.bosco05@gmail.com>
2010-12-13 14:55:15 +0530

    adding gin module

---
+++ reachout-trial/reachout-trial/src/info/arindamsharma/reachout/client/ReachoutClientModule.java
@@ -0,0 +1,15 @@
+package info.arindamsharma.reachout.client;
+
+import com.google.gwt.inject.client.AbstractGinModule;
+
+public class ReachoutClientModule extends AbstractGinModule {
+
+  @Override
+  protected void configure() {
+    // TODO Auto-generated method stub
+
+  }
+  
+  
+
+}
---
+++ reachout-trial/reachout-trial/src/info/arindamsharma/reachout/client/ReachoutInjector.java
@@ -0,0 +1,9 @@
+package info.arindamsharma.reachout.client;
+
+import com.google.gwt.inject.client.GinModules;
+import com.google.gwt.inject.client.Ginjector;
+
+@GinModules(ReachoutClientModule.class)
+public interface ReachoutInjector extends Ginjector {
+
+}
